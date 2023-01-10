40cb012bbf650204a44698bf8f20f8c123ce05c8
Wagner Fusca <wagnerfusca@gmail.com>
2023-01-10 00:51:31 -0300

    adicionado o projeto eureka

---
+++ eruka/src/main/java/com/example/eruka/ErukaApplication.java
@@ -0,0 +1,15 @@
+package com.example.eruka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ErukaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErukaApplication.class, args);
+	}
+
+}
